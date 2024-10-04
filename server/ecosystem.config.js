ca80c1912875b5f292576b50b7419417006cfe16
GeorgeKuncheria <chempumthara.g@northeastern.edu>
2024-10-04 02:59:27 -0400

    Manual changes

---
+++ server/ecosystem.config.js
@@ -0,0 +1,15 @@
+const { env } = require("process");
+
+module.exports = {
+    apps:[
+        {
+            name: "inventory-management",
+            script:"npm",
+            args:"run dev",
+            env: {
+                NODE_ENV: "development",
+                ENV_VAR1:"environment-variable",
+            },
+        }
+    ]
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -39,7 +39,7 @@ app.use("/expenses", expenseRoutes);
 
 /*SERVER */
 
-const port=process.env.PORT || 3001;
-app.listen(port,()=>{
+const port=Number(process.env.PORT) || 3001;
+app.listen(port,'0.0.0.0',()=>{
     console.log(`Server is running on port ${port}`);
 })
